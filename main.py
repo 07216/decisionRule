2583823ddddb52ffe52531964381e72ace563ec2
07216 <linzhan@mail.ustc.edu.cn>
2016-10-06 22:19:10 +0800

    Debugging

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ secondCaseInReSolve=2
 choose,t,limt,d,T = int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4]),int(sys.argv[5])
 demander = CustomizeDemand.CustomizeDemand(choose,t,limt,d,T)
 '''
-demander = CustomizeDemand.CustomizeDemand(0,5,5,1,40)
+demander = CustomizeDemand.CustomizeDemand(0,1,5,1,200)
 first = 0
 
 if first == 0:#Decision Rule Approch 
